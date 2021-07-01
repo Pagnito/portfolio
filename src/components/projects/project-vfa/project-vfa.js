cf28fe4a60f95d91a16d3b64b8503b2e9149d039
pagnito <pagnito@gmail.com>
2021-06-30 21:00:47 -0400

    working on portfolio page

---
+++ src/components/projects/project-vfa/project-vfa.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./project-vfa.scss";
+
+const ProjectVfa = () => {
+  //CAE03D
+  return (
+    <div id="project-vfa-comp">
+      <div id="project-vfa-container">
+        <div id="project-vfa-slide-down-text"></div>
+        <div id="phone-image-container">
+          <img id="phone-overlay-image" src="/assets/mobile-vfa-image-2.png" />
+          <img id="phone-image" src="/assets/phone.png" />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default ProjectVfa;
--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,24 +1,22 @@
 import React from "react";
+import ProjectVfa from './project-vfa/project-vfa';
 import "./projects.scss";
 
 const Project = () => {
   let height = window.innerHeight * .6;
 
   return (
-    <section id="project-section">
-      <div style={{height: height}} id="projects-mission-statement-board">
+    <div id="project-section">
+      <section style={{height: height}} id="projects-mission-statement-board">
         <div id="projects-mission-statement">
-          <div id="projects-mission-cool-line"></div>I believe in making an impact on people via smooth and beautiful experience.
+          <div id="projects-mission-cool-line"></div>I believe in making an impact on people via smooth, simple and beautiful experience.
         </div>
-        {/* <div id="projects-mission-circles-container">
-          <div id="projects-mission-cool-circle-1"></div>
-          <div id="projects-mission-cool-circle-2"></div>
-          <div id="projects-mission-cool-circle-3"></div>
-        </div> */}
-      </div>
-      <div id="project-container"></div>
-      <div id="project-container"></div>
-    </section>
+      
+      </section>
+      <div id="projects-screen-space-bottom"></div>
+      <ProjectVfa />
+    
+    </div>
   );
 };
 export default Project;
