a3aba5898ff02e0cb2d5e03c5ec4c6fbd43f054d
pagnito <pagnito@gmail.com>
2021-07-02 21:34:32 -0400

    added redirects

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -95,26 +95,26 @@ const Header = (props) => {
         <section id="header-right">
           <div id="header-icon-container">
             <div className="link-animation-container">
-              <a className="header-social-icon-link social-icon-face" target="_blank" id="linkedin-icon-link">
+              <a href="https://www.linkedin.com/in/pavel-yeganov-309269114/" className="header-social-icon-link social-icon-face" target="_blank" id="linkedin-icon-link">
                 <img className="header-social-icon" src="/assets/linkedin-icon.svg" />
               </a>
-              <a className="header-social-icon-link social-icon-bottom" target="_blank" id="linkedin-icon-blue-link">
+              <a href="https://www.linkedin.com/in/pavel-yeganov-309269114/" className="header-social-icon-link social-icon-bottom" target="_blank" id="linkedin-icon-blue-link">
                 <img className="header-social-icon" src="/assets/linkedin-icon-blue.svg" />
               </a>
             </div>
             <div className="link-animation-container">
-              <a className="header-social-icon-link social-icon-face" target="_blank" id="github-icon-link">
+              <a href="https://github.com/pagnito" className="header-social-icon-link social-icon-face" target="_blank" id="github-icon-link">
                 <img className="header-social-icon github-icon" src="/assets/github-logo.svg" />
               </a>
-              <a className="header-social-icon-link social-icon-bottom" target="_blank" id="github-icon-black-link">
+              <a href="https://github.com/pagnito" className="header-social-icon-link social-icon-bottom" target="_blank" id="github-icon-black-link">
                 <img className="header-social-icon github-icon" src="/assets/github-logo-black.svg" />
               </a>
             </div>
             <div className="link-animation-container">
-              <a className="header-social-icon-link social-icon-face" target="_blank" id="instagram-icon-link">
+              <a href="https://www.instagram.com/wayofthecode/" className="header-social-icon-link social-icon-face" target="_blank" id="instagram-icon-link">
                 <img className="header-social-icon instagram-icon" src="/assets/instagram-icon.svg" />
               </a>
-              <a className="header-social-icon-link social-icon-bottom" target="_blank" id="instagram-icon-gradient-link">
+              <a href="https://www.instagram.com/wayofthecode/" className="header-social-icon-link social-icon-bottom" target="_blank" id="instagram-icon-gradient-link">
                 <img className="header-social-icon instagram-icon" src="/assets/instagram-icon-gradient.svg" />
               </a>
             </div>
