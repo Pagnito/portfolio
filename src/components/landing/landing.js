62dce343de7d8c644419fd2f61e3240efd7a4ef3
pagnito <pagnito@gmail.com>
2021-07-06 11:36:43 -0400

    fixed landing typography

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -17,7 +17,7 @@ const Landing = () => {
       <div id="landing-section">
         <div className="intro-text">Hi,<span className="pavel-word" style={{color: "#8a2be2"}}>Pavel</span> is the name</div>
         <div className="intro-text">and I make the <span className="good-word" style={{color: "#EE3153", fontSize: "9vw", lineHeight: .7}}>good</span> stuff on the <span style={{color: "black"}}>internet,</span></div>
-        <div className="intro-text"><span style={{color: "#EE3153"}}>aka</span> develop <span className="sites-word" style={{color: "#8a2be2", fontSize: "7vw", lineHeight: .7}}>sites </span>
+        <div className="intro-text"><span style={{color: "#8a2be2"}}>aka</span> develop <span className="sites-word" style={{color: "#EE3153", fontSize: "7vw", lineHeight: .7}}>sites </span>
           and <span className="apps-word" style={{color: "#EE3153", fontSize: "8vw", lineHeight: .7}}>apps.</span></div>
 
         <div ref={ball} id="bounce-ball"></div>
